2f92dcbcf42bba18b35076b371f973a5f1732317
sir-omoreno <ozmoreno@gmail.com>
2021-02-02 01:50:37 -0500

    charting

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,3 +10,15 @@ var margin = {
     bottom: 80,
     left: 80
 }
+// Creating chart bounderies
+var height = svgHeight - margin.top - margin.bottom;
+var width = svgWidth - margin.left - margin.right;
+
+// Setting SVG wrapper to hold the chart
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight)
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
