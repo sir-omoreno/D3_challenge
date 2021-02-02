a8beab56b1f1f279fbe9975f2419c79906e91289
sir-omoreno <ozmoreno@gmail.com>
2021-02-02 01:34:32 -0500

    Initializing SVG

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,12 @@
 // @TODO: YOUR CODE HERE!
+// Creating SVG chart space
+var svgWidth = 1200
+var svgHeight = 600 
+
+// Setting margings:
+var margin = {
+    top: 40,
+    right: 60,
+    bottom: 80,
+    left: 80
+}
