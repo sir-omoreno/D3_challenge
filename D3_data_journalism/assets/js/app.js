db25d8f374d83f94e6520da5103309df005eb685
sir-omoreno <ozmoreno@gmail.com>
2021-02-08 23:14:12 -0500

    working with chart axis

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -21,11 +21,11 @@ var svg = d3.select("#scatter")
   .attr("height", svgHeight)
 
 var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  .attr("transform", `translate(${margin.left}, ${margin.top})`).classed("chart", true)
 
 // Time to import you data with d3 (CSV method)
 
-d3.csv("assets/data/data.csv").then(function(csv_data) {
+d3.csv("assets/data/data.csv").then(function (csv_data) {
 
   csv_data.forEach(function (item) {
     item.age = +item.age
@@ -39,11 +39,50 @@ d3.csv("assets/data/data.csv").then(function(csv_data) {
   var xScale = d3.scaleLinear()
     .domain([30, d3.max(csv_data, d => d.age)])
     .range([0, width])
-  
+
   var yScale = d3.scaleLinear()
-  .domain([30, d3.max(csv_data, d => d.smokes)])
-  .range([0, width])
+    .domain([30, d3.max(csv_data, d => d.smokes)])
+    .range([0, width])
 
-  var { bottomAxis, leftAxis } = axisFunction(xScale, yScale)
+  // var bottomAxis = d3.axisBottom(xScale)
+  // var leftAxis = d3.axisLeft(yScale)
+  var { bottomAxis, leftAxis } = newFunction(xScale, yScale);
 
-});
+  // Appending chart and created markers
+
+  chartGroup.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis)
+
+  chartGroup.append("g")
+    .call(leftAxis)
+
+  var circlesGroup = chartGroup.selectAll("circle")
+    .data(csv_data)
+    .enter()
+    .append("circle")
+    .classed("stateCircle", true)
+    .attr("cx", d => xScale(d.age))
+    .attr("cy", d => yScale(d.smokes))
+    .attr("r", "15")
+
+  chartGroup.append("g")
+    .selectAll('text')
+    .data(data)
+    .enter()
+    .append("text")
+    .classed("stateText", true)
+    .text(d => d.abbr)
+    .attr("x", d => xScale(d.age))
+    .attr("y", d => yScale(d.smokes))
+    .attr("alignment-baseline", "central")
+
+
+
+})
+function newFunction(xScale, yScale) {
+  var bottomAxis = d3.axisBottom(xScale);
+  var leftAxis = d3.axisLeft(yScale);
+  return { bottomAxis, leftAxis };
+}
+
