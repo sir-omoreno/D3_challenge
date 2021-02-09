8e4a6240821abe5612be9e306f2ec68465d3baf7
sir-omoreno <ozmoreno@gmail.com>
2021-02-08 22:20:39 -0500

    importing data

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -22,3 +22,15 @@ var svg = d3.select("#scatter")
 
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Time to import you data with d3 (CSV method)
+
+d3.csv("assets/data/data.csv").then(funtion(cvs_data) {
+  csv_data.forEach(function(item) {
+    item.age = +i.age
+    item.healthcare = +item.healthcare
+    item.obesity = +ittem.obesity
+    item.poverty = + item.poverty
+    item.smoke = + item.smokes
+  })
+})
